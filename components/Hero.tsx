697b7ce5e17851e425652ebc31bfcbbceb01c6c4
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-02 13:03:37 +0900

    implemented " Navbar  " with re-useable CustomButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,7 @@ import CustomButton from "./CustomButton"
 const Hero = () => {
 
    const handleScroll = () => {
-
    }
-
 
   return (
     <div className="hero"> 
@@ -22,6 +20,13 @@ const Hero = () => {
         
         </p>
         <CustomButton title="Explore cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+      </div>
+
+      <div className="hero__image-container">
+       <div className="hero__image"> 
+         <Image src="/hero.png" alt="hero" fill className="object-contain" />
+         <div className="hero__image-overlay"></div>
+       </div>
       </div>
     </div>
   )
--- types/index.ts
+++ types/index.ts
@@ -3,7 +3,8 @@ import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
     title: string;
-    containerStyle?: string;
+    containerStyles?: string;
     handleClick?: 
     MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
 }
