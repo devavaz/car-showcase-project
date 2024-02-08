f8cfc15c5d2e89eff41471865822f2a1d1d5aae2
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-09 00:47:11 +0900

    implemented and start functioning Custom filter

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+import { title } from "process";
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
@@ -28,4 +29,25 @@ export interface CarProps {
     model: string;
     transmission: string;
     year: number;
+}
+
+
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+    
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,21 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
     const headers = {
         'X-RapidAPI-Key': 'f824c76c08msh3e3d27b52df2b54p15f177jsna48a03bcd68a',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     };
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
-        headers: headers,
-    });
+
+    const { manufacturer, year, fuel, limit, model } = filters;
+
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        {
+          headers: headers,
+        }
+      );
 
     const result = await response.json();
     return result;
